4036d08faa308bc23789fd144ec50ad65a0432d3
phunguyen1195 <phuchau1110@gmail.com>
2019-11-21 14:22:48 -0800

    fixed v vector, pretty print matrices

--- mathdatasci.py
+++ mathdatasci.py
@@ -1,21 +1,28 @@
 import numpy as np
 from scipy import linalg
 from discreteMarkovChain import markovChain
-
+from sympy import *
 
 
 class mathdatasci:
-    def __init__(self, P, l):
+    def __init__(self, P, l, auto_l = True):
         self.P = P
         self.l = l
-        self.v = np.ndarray((2,2))
-        self.u = np.ndarray((2,2))
+        self.v = np.empty(0)
+        self.u = np.empty(0)
         self.Pl = np.empty(0)
         self.Pl_normalize_stochastic = np.empty(0)
-        self.eigen_stationarydist = {}
-        self.get_v()
-        self.get_u()
-        self.get_Pnl()
+        self.eigen_stationarydist_Pl = {}
+        self.eigen_stationarydist_normalize_Pl = {}
+        self.auto_l = auto_l
+        if self.auto_l == True:
+            self.get_v()
+            self.get_u()
+            self.get_Pnl()
+        else:
+            self.v = l
+            self.get_u()
+            self.get_Pnl()
         self.normalize_stochastic_matrix()
     
     def get_v (self):
@@ -43,9 +50,30 @@ class mathdatasci:
             Pl_normalize_stochastic[i]  =Pl_normalize_stochastic[i]* ((k)**(-1))
         self.Pl_normalize_stochastic = Pl_normalize_stochastic
 
-    def get_eigen_stationarydist(self):
+    def get_eigen_stationarydist_normalize_Pl(self):
         w, vl, vr = linalg.eig(self.Pl_normalize_stochastic, left=True)
         mc = markovChain(self.Pl_normalize_stochastic)
         mc.computePi('linear')
-        self.eigen_stationarydist =  {'eigen values': w, 'eigen vector': vl,'stationary distribution': mc.pi}
-        return self.eigen_stationarydist
+        self.eigen_stationarydist_normalize_Pl =  {'eigen values': np.around(w, 3), 'eigen vector': np.around(vr, 3),'stationary distribution': np.around(mc.pi,3)}
+        return self.eigen_stationarydist_normalize_Pl
+
+    def get_eigen_stationarydist_Pl(self):
+        w, vl, vr = linalg.eig(self.Pl_normalize_stochastic, left=True)
+        mc = markovChain(self.Pl_normalize_stochastic)
+        mc.computePi('linear')
+        self.eigen_stationarydist_Pl =  {'eigen values': np.around(w, 3), 'eigen vector': np.around(vr, 3),'stationary distribution': np.around(mc.pi,3)}
+        return self.eigen_stationarydist_Pl
+
+    def getv (self):
+        return np.around(self.v, 3)
+    
+    def getu (self):
+        return np.around(self.u, 3)
+
+    def getPl (self):
+        return np.around(self.Pl, 3)
+
+    def getPl_normalize_stochastic (self):
+        return np.around(self.Pl_normalize_stochastic,3)
+
+
